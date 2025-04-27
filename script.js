0763aec1d759c46f329d5f53451772408a263811
N&G Interprises <neginterprises@gmail.com>
2025-04-27 10:36:02 -0300

    Inserindo a lógica com JavaScript, chamando as funções definidas dentro do html, como gerar senha, selecionar tipos de senhas e opção de copiar com um click

---
+++ script.js
@@ -0,0 +1,63 @@
+
+
+const sliderElement = document.getElementById("slider");
+const spanElement = document.getElementById("valor");
+const buttonElement = document.getElementById("button");
+const containerPassword = document.getElementById("container-password");
+const passwordElement = document.getElementById("password");
+
+spanElement.innerHTML = sliderElement.value;
+let senha = "";
+
+sliderElement.oninput = function() {
+    spanElement.innerHTML = this.value;
+}
+
+function generatePassword() {
+    const passLength = sliderElement.value;
+    const lowercase = document.getElementById("checkbox-lowercase").checked;
+    const uppercase = document.getElementById("checkbox-uppercase").checked;
+    const number = document.getElementById("checkbox-number").checked;
+    const symbol = document.getElementById("checkbox-symbol").checked;
+
+
+  
+
+    if (lowercase) {
+        senha += "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    if (uppercase) {
+        senha += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+
+    if (number) {
+        senha += "0123456789";
+    }
+
+    if (symbol) {
+        senha += "!@#$%&*()_+-=[]{}|;:,.<>?";
+    }
+
+    if (senha === "") {
+        alert("Por favor, escolha pelo menos uma opção.");
+        return;
+    }    
+
+    let password = "";
+    for (let i = 0; i < passLength; i++) {
+        const randomIndex = Math.floor(Math.random() * senha.length);
+        password += senha.charAt(randomIndex);
+    }
+
+    passwordElement.innerText = password;
+    containerPassword.classList.remove("hide");
+    spanElement.innerText = sliderElement.value;
+}
+    function copyPassword() {    
+    navigator.clipboard.writeText(passwordElement.innerText);
+    alert("Senha copiada com sucesso!");
+    
+    }
+    
+    
